b5e4d739576a9f8015400437c64bad8397fbe6ae
Ujjwal Pandey <ujjwalpandey922@gmail.com>
2025-07-16 15:46:41 +0530

    build fix

--- src/pages/collections/BurialServiceReceiptPage.jsx
+++ src/pages/collections/BurialServiceReceiptPage.jsx
@@ -229,7 +229,7 @@ function BurialServiceReceiptPage() {
         title={
           selectedReceipt
             ? 'Edit Burial Service Receipt'
-            : 'New Burial Service Receipt'
+            : 'New Burial Service Receipt '
         }
       >
         <BurialServiceReceiptForm
